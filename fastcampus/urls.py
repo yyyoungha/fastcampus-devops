0d58f388ba3aa5ba0dd7b10f79a54afde6a51974
Young <yyyoungha@gmail.com>
2022-10-05 08:18:48 +0000

    Add delivery backend

--- fastcampus/urls.py
+++ fastcampus/urls.py
@@ -19,6 +19,7 @@ from django.urls import path, include
 urlpatterns = [
     path('order/', include('order.urls')),
     path('boss/', include('boss.urls')),
+    path('delivery/', include('delivery.urls')),
     path('admin/', admin.site.urls),
     path('api-auth/', include('rest_framework.urls', namespace='rest_framework'))
 ]
