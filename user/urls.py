ab2362df7607688a44b7006e7681537a6a2fad70
Young <yyyoungha@gmail.com>
2022-10-06 06:45:47 +0000

    Add user login

--- user/urls.py
+++ user/urls.py
@@ -2,6 +2,7 @@ from django.urls import path
 from user import views
 
 urlpatterns = [
-    path('user/', views.user, name="user")
+    path('user/', views.user, name="user"),
+    path('login/', views.login, name="login")
 ]
 
--- user/views.py
+++ user/views.py
@@ -18,3 +18,16 @@ def user(request):
             serializer.save()
             return JsonResponse(serializer.data, status=201)
         return JsonResponse(serializer.errors, status=400)
+
+@csrf_exempt
+def login(request):
+    if request.method == 'POST':
+        try:
+            name = request.POST['name']
+            User.objects.all().get(name=name)
+            return render(request, 'user/success.html')
+        except:
+            return render(request, 'user/fail.html')
+        
+    elif request.method =='GET':
+        return render(request, 'user/login.html') 
